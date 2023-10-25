cf091fb6f5e72bcb5b11c08eade4f8c0ca785144
Prafful Raghaw <123306750+PRAFFULRAGHAW@users.noreply.github.com>
2023-10-25 15:36:34 +0530

    Create DataTypesInJava.java

---
+++ code/JavaBasic/DataTypesInJava.java
@@ -0,0 +1,31 @@
+//Basic Data Types in Java
+
+public class DataTypesInJava {
+    public static void main(String[] args) {
+
+        int money=1000;
+        System.out.println(money);
+        money=5000;
+        System.out.println(money);
+        boolean flag = true;
+        int a = 123;
+        short s = 1200;
+        byte b = 120;
+        long l = 12345678;
+        float f = 1.234f;
+        double d = 123.45678;
+        char ch = 'A';
+
+        System.out.println("boolean value is " +flag);
+        System.out.println("int value is " +a);
+        System.out.println("short value is " +s);
+        System.out.println("byte value is " +b);
+        System.out.println("long value is " +l);
+        System.out.println("float value is " +f);
+        System.out.println("double value is " +d);
+        System.out.println("char value is " +ch);
+
+        
+    }
+    
+}

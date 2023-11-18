29e77f819f7ad0cad5bdc42cb55a8a0b6c91a2b8
Prafful Raghaw <123306750+PRAFFULRAGHAW@users.noreply.github.com>
2023-11-18 17:28:56 +0530

    Add files via upload

--- code/Array/MultiDimensional Array/PascalTriangle.java
+++ code/Array/MultiDimensional Array/PascalTriangle.java
@@ -5,7 +5,7 @@ public class PascalTriangle {
     static void pascalTriangle(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + "\t");
+                System.out.print(matrix[i][j] + " \t ");
             }
             System.out.println();
         }
